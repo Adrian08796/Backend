fb1121fa1327a77d73f5aa19490e9424e35ce3d8
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-08-22 14:41:50 +0200

    refactored workout history functionality

--- models/WorkoutProgress.js
+++ models/WorkoutProgress.js
@@ -2,19 +2,69 @@
 
 const mongoose = require('mongoose');
 
+const SetSchema = new mongoose.Schema({
+  weight: {
+    type: Number,
+    required: true
+  },
+  reps: {
+    type: Number,
+    required: true
+  },
+  completedAt: {
+    type: Date,
+    required: true
+  },
+  skippedRest: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const ExerciseProgressSchema = new mongoose.Schema({
+  exercise: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Exercise',
+    required: true
+  },
+  sets: [SetSchema],
+  notes: String
+});
+
 const WorkoutProgressSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  data: {
-    type: mongoose.Schema.Types.Mixed,
+  plan: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'WorkoutPlan',
+    required: true
+  },
+  exercises: [ExerciseProgressSchema],
+  currentExerciseIndex: {
+    type: Number,
+    default: 0
+  },
+  startTime: {
+    type: Date,
     required: true
   },
   lastUpdated: {
     type: Date,
     default: Date.now
+  },
+  totalPauseTime: {
+    type: Number,
+    default: 0
+  },
+  skippedPauses: {
+    type: Number,
+    default: 0
+  },
+  lastSetValues: {
+    type: mongoose.Schema.Types.Mixed
   }
 });
 
