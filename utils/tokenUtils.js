8c6824be30e1d4926dc103d5364af2e98d607594
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-08-25 20:22:42 +0200

    implemented token refresh bug fix

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: process.env.JWT_ACCESS_EXPIRATION,
+    expiresIn: process.env.JWT_ACCESS_EXPIRATION
   });
 };
 
 const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRATION,
+    expiresIn: process.env.JWT_REFRESH_EXPIRATION
   });
 };
 
