f789ec5be10a73d5cebdcf184fa8639f80a206cb
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-09-02 13:42:38 +0200

    token refresh tests

--- models/User.js
+++ models/User.js
@@ -1,12 +1,25 @@
 // models/User.js
+
 const mongoose = require('mongoose');
+
+const MAX_ACTIVE_REFRESH_TOKENS = 5;
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  blacklistedTokens: [{ type: String }],
-  blacklistedTokensTimestamps: [{ type: Date }]
+  activeRefreshTokens: [{ type: String }]
 });
+
+UserSchema.methods.addRefreshToken = function(token) {
+  this.activeRefreshTokens.push(token);
+  if (this.activeRefreshTokens.length > MAX_ACTIVE_REFRESH_TOKENS) {
+    this.activeRefreshTokens.shift(); // Remove the oldest token
+  }
+};
+
+UserSchema.methods.removeRefreshToken = function(token) {
+  this.activeRefreshTokens = this.activeRefreshTokens.filter(t => t !== token);
+};
 
 module.exports = mongoose.model('User', UserSchema);
